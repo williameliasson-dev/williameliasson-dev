50f36a9a388bead98da75e867dd67b72d5a71857
“Apspelet33” <williamstarwars1@hotmail.com>
2023-01-08 00:11:53 +0100

    inital scale

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,10 @@ const Head: React.FC<HeadProps> = ({}) => {
   return (
     <>
       <title>williameliasson-dev</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+      />
     </>
   );
 };
